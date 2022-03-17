5a290dbebb1552fb78e43b620793b878f89f35c4
mpowers47 <93107994+mpowers47@users.noreply.github.com>
2022-03-17 01:19:50 -0400

    Create CheckSufficientBalanceorOD.py

---
+++ CheckSufficientBalanceorOD.py
@@ -0,0 +1,16 @@
+balance = 20.0
+price = 19.0
+overdraft_protection = True
+
+#
+#Write some code below that will print True if the customer
+#can make the purchase given their balance, the purchase
+#price, and whether or not they have overdraft protection.
+#Specifically, the result should be True if balance is greater
+#than or equal to price or if overdraft_protection is True,
+#and False if neither of these are true.
+
+
+#Add your code here!
+
+print ((balance >= price) or (overdraft_protection))
