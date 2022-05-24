b5e7f50c870b0ddcf616cce9ca1196b884c4daf5
mpowers47 <93107994+mpowers47@users.noreply.github.com>
2022-05-24 18:54:00 -0400

    Create Valentines.py

---
+++ Valentines.py
@@ -0,0 +1,14 @@
+if years == 0 and months == 0 and days == 0:
+    print("yacht")
+
+elif years >= 4:
+    print("dog")
+    
+elif years >= 1 and years < 4:
+    print("watch")
+      
+elif months >= 6 and years < 1:
+    print("concert tickets")
+            
+elif days >= 1 or months < 6 and years < 1:
+    print("candy")
