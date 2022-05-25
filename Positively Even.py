47b8047b00c9b398a1138af82b8729b18dc873d7
mpowers47 <93107994+mpowers47@users.noreply.github.com>
2022-05-24 20:27:27 -0400

    Create Positively Even.py

---
+++ Positively Even.py
@@ -0,0 +1,8 @@
+if my_int % 2 == 0 and my_int > 0:
+    print ("Evenly positive")
+if my_int % 2 == 0 and my_int < 0:
+    print ("Evenly negative")   
+if my_int % 2 != 0 and my_int > 0:
+    print ("Oddly positive")
+if my_int % 2 != 0 and my_int < 0:
+    print ("Oddly negative")
