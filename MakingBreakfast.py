f084454a378fa37314b3f7b91cd295d754e0f4ed
mpowers47 <93107994+mpowers47@users.noreply.github.com>
2022-06-14 23:56:32 -0400

    Create MakingBreakfast.py

---
+++ MakingBreakfast.py
@@ -0,0 +1,57 @@
+egg = True
+milk = True
+butter = True
+flour = True
+
+#You may modify the lines of code above, but don't move them!
+#When you Submit your code, we'll change these lines to
+#assign different values to the variables.
+
+#Imagine you're deciding what you want to cook. The boolean
+#variables above state whether or not you have each of those
+#four ingredients.
+#
+#Here are the dishes you know how to cook and their
+#ingredients:
+#
+# 1. pancakes: egg, milk, butter, flour
+# 2. omelette: egg, milk, butter
+# 3. custard: egg, milk
+# 4. poached eggs: egg
+#
+#The list above is also a priority list. If you have the
+#ingredients for pancakes, you'll make pancakes instead of
+#any of the other dishes. If you're missing flour but have
+#the other ingredients, you'll make an omlette. You'll only
+#make poached eggs if the only ingredient you have is eggs.
+#
+#Complete the program below such that it prints which dish
+#you'll make based on the ingredients you have handy. All
+#the dishes should appear exactly as shown above: all lower
+#case, spelled the same way. If you do not have the
+#ingredients to make any of these dishes, then print the
+#text "Go to the store!"
+
+
+#Add your code here!
+
+#egg = True
+#milk = True
+#butter = True
+#flour = True
+
+#pancakes = egg + milk + butter + flour
+#omelette = egg + milk + butter
+#custard = egg + milk
+#poached_eggs = egg
+
+if (egg and milk and butter and flour) is True:
+    print("pancakes")
+elif (egg and milk and butter) is True and (flour) is False:
+    print("omelette")
+elif (egg and milk) is True and (butter and flour) is False:
+    print("custard")
+elif (egg) is True and (butter and flour and milk) is False:
+    print("poached eggs")
+else:
+    print("Go to the store!")
